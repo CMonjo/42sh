38bd10a4f4401e9e7820fc536efcf0a90b8df5fc
Camille <monjocamille@gmail.com>
2018-04-29 21:38:27 +0200

    Fix when the bind have one space

--- src/builtins/alias/alias.c
+++ src/builtins/alias/alias.c
@@ -7,20 +7,21 @@
 
 #include "main.h"
 
-alias_t *alias_add_node(char **str, char *long_str, char *parenthesis)
+alias_t *alias_add_node(char **str, char *long_str, char *parents)
 {
 	alias_t *tmp = malloc(sizeof(alias_t));
 
 	tmp->bind = str[1];
 	tmp->command_bind = my_strdup(long_str);
-	tmp->command_display = my_strdup(alias_check_string(str, parenthesis));
+	tmp->command_display =
+	my_strdup(alias_check_string(long_str, parents));
 	tmp->next = NULL;
 	return(tmp);
 }
 
-void alias_add(env_st_t *env_st, char **str, char *long_str, char *parenthesis)
+void alias_add(env_st_t *env_st, char **str, char *long_str, char *parents)
 {
-	alias_t *new = alias_add_node(str, long_str, parenthesis);
+	alias_t *new = alias_add_node(str, long_str, parents);
 
 	if (env_st->alias == NULL)
 		env_st->alias = new;
@@ -30,32 +31,33 @@ void alias_add(env_st_t *env_st, char **str, char *long_str, char *parenthesis)
 	}
 }
 
-void alias_fill(env_st_t *env_st, char **str, char *long_str, char *parenthesis)
+void alias_fill(env_st_t *env_st, char **str, char *long_str, char *parents)
 {
 	alias_t *tmp = env_st->alias;
 
 	while (tmp != NULL) {
 		if (my_strcmp(tmp->bind, str[1]) == 0) {
 			tmp->command_bind = my_strdup(long_str);
-			tmp->command_display = my_strdup(alias_check_string(str, parenthesis));
+			tmp->command_display =
+			my_strdup(alias_check_string(long_str, parents));
 			return;
 		}
 		tmp = tmp->next;
 	}
-	alias_add(env_st, str, long_str, parenthesis);
+	alias_add(env_st, str, long_str, parents);
 
 }
 
 int alias(char **array, UNUSED char **envp, env_st_t *env_st)
 {
-	char *parenthesis = alias_parse_parenthesis(array);
 	char *long_str = alias_parse(array);
+	char *parents = alias_parse_parenthesis(array);
 
 	if (array[1] == NULL)
 		alias_display(env_st);
 	else if (array[2] == NULL)
 		alias_compare(env_st, array[1]);
 	else
-		alias_fill(env_st, array, long_str, parenthesis);
+		alias_fill(env_st, array, long_str, parents);
 	return (0);
 }

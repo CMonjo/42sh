8a1cdd989d3a3e2b07c8a7f4c30bfebbb3dfca59
Nicolas Albiges <nalbiges22@gmail.com>
2018-05-04 20:57:25 +0200

    [FIX]: removed the printf

--- src/builtins/echo/echo.c
+++ src/builtins/echo/echo.c
@@ -28,7 +28,6 @@ int	echo(char **arr, UNUSED char **envp, UNUSED env_st_t *env_st)
 {
 	if (arr[1] == NULL)
 		return (0);
-	my_printf("WALLA\n");
 	for (int ct = 1; arr[ct] != NULL; ct ++) {
 		if (arr[ct][0] == 39 || arr[ct][0] == 34)
 			skip_print_quotes(arr, ct, 1, arr[ct][0]);

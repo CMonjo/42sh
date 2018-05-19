40a9f4be114b31e92cc71c4021d9b3fd30c56197
Nicolas Albiges <nalbiges22@gmail.com>
2018-05-19 23:53:41 +0200

    [FEAT]:  fixed the job error

--- src/builtins/jobs/jobs.c
+++ src/builtins/jobs/jobs.c
@@ -9,6 +9,12 @@
 
 int	jobs(char **arr, UNUSED char **envp, UNUSED env_st_t *env_st)
 {
-	my_putstr_err(arr[0], ": No current job.\n");
+	if (isatty(0) != 1) {
+		env_st->status = 1;
+		my_putstr_err(arr[0], "No job control in this shell.\n");
+	} else {
+		env_st->status = 1;
+		my_putstr_err(arr[0], ": No current job.\n");
+	}
 	return (0);
 }

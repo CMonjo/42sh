0cba36559026e335da1a70e15363dd9ebf812e3a
Nicolas Albiges <nalbiges22@gmail.com>
2018-05-20 02:22:36 +0200

    [FEAT]:  fixed the if

--- src/builtins/if/if.c
+++ src/builtins/if/if.c
@@ -87,21 +87,21 @@ int	if_build(char **arr, char **envp, env_st_t *env_st)
 {
 	int b = 0;
 	char *sep = malloc(sizeof(char) * 1);
-	int ct = 3;
+	int ctb = 3;
 
 	if (error_if(arr, env_st) == 1 || error_if_syntax(arr, env_st) == 1)
 		return (1);
 	if (arr[1][0] != '(')
 		return (0);
 	sep[0] = '\0';
-	while (arr[ct + 1][0] != ')') {
-		sep = my_strcat(sep, arr[ct], 0);
-		ct ++;
+	while (arr[ctb + 1][0] != ')') {
+		sep = my_strcat(sep, arr[ctb], 0);
+		ctb ++;
 	}
-	//printf("no error    ARR   :   %s\n", sep);
+	printf("no error    SEP   :   %s  CT   :   %d     ARR[ct] :   %s\n", sep, ctb, arr[ctb]);
 	for (int ct = 0; ct != 5; ct ++)
 		if (my_strcmp(sep, tab_if_sep[ct].name) == 0) {
-			if ((tab_if_sep[ct].name_str)(arr[2], arr[ct]) == 0)
+			if ((tab_if_sep[ct].name_str)(arr[2], arr[ctb]) == 0)
 				main_b_tree(if_command(arr), env_st, 0, 1);
 			b ++;
 		}

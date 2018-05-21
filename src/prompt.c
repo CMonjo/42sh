58009a0d9c363ecec17bed824f35b1fc0448262f
Nicolas Albiges <nalbiges22@gmail.com>
2018-05-21 18:08:19 +0200

    [FEAT]:  fixed the builtins

--- src/prompt.c
+++ src/prompt.c
@@ -33,7 +33,7 @@ int	check_bult_in(char *str)
 {
 	char *bul_in[] = {"cd", "env", "exit", "setenv", "unsetenv",
 	"alias", "echo", "repeat", "which", "where", "set",
-	"unset", "foreach", "fg", "bg"};
+	"unset", "foreach", "fg", "bg", "if"};
 
 	for (int ct = 0; ct != 16; ct ++) {
 		if (my_strcmp(str, bul_in[ct]) == 0)

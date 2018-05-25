b010a7526ac8e1476d97b41ba6ea82830d340146
Clément <clement.chaptal@epitech.eu>
2018-05-25 23:34:16 +0200

    yes_modif

--- src/yes.c
+++ src/yes.c
@@ -11,6 +11,12 @@ int	yes(char **str, UNUSED char **envp, UNUSED env_st_t *env_st)
 {
 	int len = my_strlen(str[1]);
 
+	if (str[1] == NULL) {
+		str[1] = malloc(sizeof(char) * 2);
+		str[1][0] = 'y';
+		str[1][1] = '\0';
+		len = 1;
+	}
 	while (1) {
 		write(1, str[1], len);
 		my_putchar('\n');

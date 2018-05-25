eeccfa25ed5b29bdb062e93add27ba85b04f684b
Clément <clement.chaptal@epitech.eu>
2018-05-25 17:31:48 +0200

    norme_fichier_error_parent.c

--- src/error/error_parent.c
+++ src/error/error_parent.c
@@ -25,27 +25,21 @@ int	error_null_parent(char **command)
 	return (0);
 }
 
-int	error_parent(char *command)
+void	error_parent_next(int *parent_left, int *parent_right, char *command)
 {
-	int parent_left = 0;
-	int parent_right = 0;
-
 	for (int ct = 0; command[ct] != '\0'; ct ++) {
 		if (command[ct] == ')')
-			parent_right ++;
+			*parent_right += 1;
 		if (command[ct] == '(')
-			parent_left ++;
+			*parent_left += 1;
 		if (command[ct] == 34 || command[ct] == 39) {
 			ct ++;
 			error_backstick_quote(command, command[ct - 1], &ct);
 		}
 	}
-	if (parent_left == 0 && parent_right == 0)
-		return (0);
-	if (parent_left == 0) {
-		my_printf("Too many %c's.\n", ')');
-		return (1);
-	}
+}
+int	error_parent_next_next(int parent_left, int parent_right)
+{
 	if (parent_right == 0) {
 		my_printf("Too many %c's.\n", '(');
 		return (1);
@@ -58,8 +52,20 @@ int	error_parent(char *command)
 		my_printf("Too many %c's.\n", ')');
 		return (1);
 	}
-//	if (error_null_parent(word_array(command)) == 1) {
-//		return (1);
-//	}
 	return (0);
+}
+
+int	error_parent(char *command)
+{
+	int parent_left = 0;
+	int parent_right = 0;
+
+	error_parent_next(&parent_left, &parent_right, command);
+	if (parent_left == 0 && parent_right == 0)
+		return (0);
+	if (parent_left == 0) {
+		my_printf("Too many %c's.\n", ')');
+		return (1);
+	}
+	return (error_parent_next_next(parent_left, parent_right));
 }

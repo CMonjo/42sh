48a66775922a17f5a74cbd00648f93807b1ccc19
Camille <monjocamille@gmail.com>
2018-05-27 18:06:33 +0200

    [HOTFIX] Norm on bonus.

--- bonus/src/bonus.c
+++ bonus/src/bonus.c
@@ -94,7 +94,7 @@ int	main_loop(char **envp, int end, char *str)
 int	main(int ac, char **av, char **envp)
 {
 	if (ac == 2 && my_strcmp(av[1], "-g") == 0)
-		return(main_graph(envp));
+		return (main_graph(envp));
 	else if (ac == 1)
 		return (main_loop(envp, 0, NULL));
 }
